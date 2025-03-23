053ea8e9d743952ba00e5c0cfe376aa1b3ce0132
Daniel Kostenko <danielkostenko2005@gmail.com>
2025-03-22 22:44:14 -0400

    view password fnc

--- employees/script.js
+++ employees/script.js
@@ -53,3 +53,11 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
         console.error('Login error:', error);
     });
 });
+
+// view password
+document.getElementById('togglePassword').addEventListener('click', function () {
+    const passwordField = document.getElementById('password');
+    const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordField.setAttribute('type', type);
+    this.classList.toggle('fa-eye-slash');
+});
